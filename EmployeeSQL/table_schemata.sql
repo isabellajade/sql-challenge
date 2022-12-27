faa3dd88cda1f0740c736ecc6d52798634530170
isabellajade <isabellajadet@gmail.com>
2022-12-26 22:16:08 -0600

    improved table schema

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,63 +1,54 @@
+DROP TABLE IF EXISTS departments cascade;
 
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "dept_name" VARCHAR(80)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE departments (
+    dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(80) NOT NULL
 );
 
-CREATE TABLE "titles" (
-    "title_id" VARCHAR(10)   NOT NULL,
-    "title" VARCHAR(80)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
+DROP TABLE IF EXISTS titles cascade;
+
+CREATE TABLE titles (
+    title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+    title VARCHAR(80) NOT NULL
 );
 
-CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR(10)   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(80)   NOT NULL,
-    "last_name" VARCHAR(80)   NOT NULL,
-    "sex" CHAR(1)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+DROP TABLE IF EXISTS employees cascade;
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY NOT NULL,
+    emp_title_id VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL
+DROP TABLE IF EXISTS dept_emp cascade;
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	CONSTRAINT dept_emp_prim PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" INT   NOT NULL
+DROP TABLE IF EXISTS dept_manager cascade;
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	CONSTRAINT dept_manager_prim PRIMARY KEY (dept_no, emp_no)
 );
 
-CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL
+DROP TABLE IF EXISTS salaries cascade;
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    salary INT NOT NULL
 );
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
